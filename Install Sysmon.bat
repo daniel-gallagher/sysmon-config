0841a201b5e0378aea72669dc98b0ca2a87ff779
Daniel Gallagher <daniel@dangsecurity.com>
2017-03-08 13:47:29 -0500

    Updated batch file to use personal config
    Also opted to use the 32bit version of Sysmon for better compatibility.

--- Install Sysmon.bat
+++ Install Sysmon.bat
@@ -1,11 +1,11 @@
 @echo off
 mkdir C:\ProgramData\sysmon
-cd C:\ProgramData\sysmon\
 echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\sysmon64.exe')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\ProgramData\sysmon\sysmon.exe')"
 echo [+] Downloading Sysmon config...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ion-storm/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-sysmon64.exe -accepteula -i sysmonconfig-export.xml
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/daniel-gallagher/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
+echo [+] Installing Sysmon...
+C:\ProgramData\sysmon\sysmon.exe -accepteula -i sysmonconfig-export.xml
 echo [+] Sysmon Successfully Installed!
 timeout /t 10
 exit
